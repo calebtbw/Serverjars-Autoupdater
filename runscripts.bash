e6f70ffa1ecfd7aaa821a20e866f9eeed8be2577
Sobriety S <admin@spherosolutions.net>
2022-02-21 10:20:56 +0800

    first commit

---
+++ runscripts.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python3 /versions/scripts/serverjars/bungeecord/bungeecord-latest.py &
+python3 /versions/scripts/serverjars/bungeecord/waterfall-latest.py & 
+python3 /versions/scripts/serverjars/geyser/floodgate-spigot.py &
+python3 /versions/scripts/serverjars/geyser/geyser-spigot.py & 
+python3 /versions/scripts/serverjars/geyser/paper-geyser.py & 
+python3 /versions/scripts/serverjars/nukkitx/nukkitx-latest.py & 
+python3 /versions/scripts/serverjars/paper/paper-latest.py & 
+python3 /versions/scripts/serverjars/purpur/purpur-latest.py &
+python3 /versions/scripts/serverjars/spigot/spigot-latest.py &
+python3 /versions/scripts/serverjars/tuinity/tuinity-latest.py &
+python3 /versions/scripts/serverjars/vanilla/vanilla-latest.py & 
+python3 /versions/scripts/serverjars/vanilla/snapshot-latest.py &
---
+++ tuinity/tuinity-latest.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3.8
+
+import os
+import wget
+import json
+import time
+import hashlib
+import requests
+
+
+def post_to_slack(message):
+    webhook_url = "some-webhook-url-here"
+
+    encoded_data = json.dumps({'text': message}).encode('utf-8')
+    response = requests.post(
+        webhook_url,
+        data=encoded_data
+    )
+    # print(str(response.status_code))
+
+jar_name = "Tuinity"
+filepath = "/home/minecraft/multicraft/jar/tuinity-latest.jar"
+dl_url = "https://serverjars.com/api/fetchJar/tuinity"
+ver_url = "https://serverjars.com/api/fetchLatest/tuinity"
+
+response = requests.get(ver_url).content
+currentHash = hashlib.sha224(response).hexdigest()
+
+ver_response = requests.get(ver_url)
+data = ver_response.json()
+version = data["response"]["version"]
+
+# post_to_slack(f"Fetching latest {jar_name} jar...")
+time.sleep(10)
+
+if not os.path.exists(filepath):
+    wget.download(dl_url, filepath)
+    post_to_slack(f"{jar_name} {version} has been downloaded!")
+
+while True:
+    try:
+        response = requests.get(ver_url).content
+        currentHash = hashlib.sha224(response).hexdigest()
+
+        time.sleep(7200)
+
+        response = requests.get(ver_url).content
+        newHash = hashlib.sha224(response).hexdigest()
+
+        if newHash == currentHash:
+            # post_to_slack(f"No updates for {jar_name}.")
+            continue
+
+        else:
+            if os.path.exists(filepath):
+                os.remove(filepath)
+                # post_to_slack(f"Removing old {jar_name} jar...")
+
+                wget.download(dl_url, filepath)
+                post_to_slack(f"{jar_name} has been updated to {version}!")
+            
+            response = requests.get(ver_url).content
+            currentHash = hashlib.sha224(response).hexdigest()
+
+            time.sleep(7200)
+            continue
+
+    except Exception as e:
+        post_to_slack(f"Error occured in {os.path.basename(__file__)}!")
